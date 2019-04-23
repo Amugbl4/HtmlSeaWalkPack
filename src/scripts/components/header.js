c636c3dcff3e360908bdc0a95daf8838c26b9876
Dmitry Mekerov <dima.mekeroff@gmail.com>
2019-04-23 14:31:55 +0300

    header js, modal js

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -1,5 +1,23 @@
-var menu = document.querySelector('.header__nav-toggle');
+var menuBtn = document.querySelector('.header__nav-toggle');
 var body = document.querySelector('body');
-menu.addEventListener('click', function () {
+var menu = document.querySelector('.main-nav__list');
+
+document.addEventListener('click', function () {
+    body.classList.remove('menu-opened')
+});
+
+document.addEventListener('keydown', function (event) {
+    let key = event.keyCode;
+    if (key === 27) {
+        body.classList.remove('menu-opened');
+    }
+})
+
+menuBtn.addEventListener('click', function (event) {
     body.classList.toggle('menu-opened');
-}) 
+    event.stopPropagation();
+});
+
+menu.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,12 +1,37 @@
-var card = document.querySelector('.card');
+var cards = document.querySelectorAll('.card');
 var overlay = document.querySelector('.modal-overlay');
 var modal = document.querySelector('.modal-card');
-var closebtn = document.querySelector('.modal-card__close');
-card.addEventListener('click', function(){
-    modal.classList.add('modal-card--open');
-    overlay.classList.add('modal-overlay--show');
-});
-closebtn.addEventListener('click', function(){
+var closeBtn = document.querySelector('.modal-card__close');
+
+overlay.addEventListener('click', function () {
     modal.classList.remove('modal-card--open');
     overlay.classList.remove('modal-overlay--show');
 });
+
+closeBtn.addEventListener('click', function () {
+    modal.classList.remove('modal-card--open');
+    overlay.classList.remove('modal-overlay--show');
+});
+
+document.addEventListener('keydown', function (event) {
+    let key = event.keyCode;
+    if (key === 27) {
+        modal.classList.remove('modal-card--open');
+        overlay.classList.remove('modal-overlay--show');
+    }
+});
+
+for (let i = 0; i < cards.length; i++) {
+    console.log('cards', i);
+    cards[i].addEventListener('click', function () {
+        console.log('let i', i);
+        console.log(cards[i]);
+        modal.classList.add('modal-card--open');
+        overlay.classList.add('modal-overlay--show');
+    });
+}
+
+// $('.card').click(function () {
+//     modal.classList.add('modal-card--open');
+//     overlay.classList.add('modal-overlay--show');
+// });
